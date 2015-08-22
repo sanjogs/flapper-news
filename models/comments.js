d907d1ef8e22d539d117b7b6dd91adf3d939c13f
sanjogsharma <sharma>
2015-08-22 15:48:10 -0400

    a

--- models/comments.js
+++ models/comments.js
@@ -5,7 +5,7 @@ var CommentSchema = new mongoose.Schema({
   author: String,
   upvotes: {type: Number, default: 0},
   post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' }
-});
+});  
 
 CommentSchema.methods.upvote = function(cb) {
   this.upvotes += 1;
